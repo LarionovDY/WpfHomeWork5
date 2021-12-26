279e92b1f043c9ebdccae83982f58fd2f526f7a3
Dmitriy Larionov <larionov.d.y@gmail.com>
2021-12-26 23:45:43 +0300

    Добавьте файлы проекта.

---
+++ 01_TextEditor/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _01_TextEditor
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            if (textBox != null)
+                textBox.FontFamily = new FontFamily(fontName);
+        }
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            int fontSize = Convert.ToInt32(((sender as ComboBox).SelectedItem as TextBlock).Text);
+            if (textBox != null)
+                textBox.FontSize = fontSize;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FontWeight fontWeight = textBox.FontWeight;
+            if (textBox != null && fontWeight != FontWeights.Bold)
+            {
+                textBox.FontWeight = FontWeights.Bold;
+            }
+            else if (textBox != null && fontWeight == FontWeights.Bold)
+            {
+                textBox.FontWeight = FontWeights.Normal;
+            }
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            FontStyle fontStyle = textBox.FontStyle;
+            if (textBox != null && fontStyle != FontStyles.Italic)
+            {
+                textBox.FontStyle = FontStyles.Italic;
+            }
+            else if (textBox != null && fontStyle == FontStyles.Italic)
+            {
+                textBox.FontStyle = FontStyles.Normal;
+            }
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            TextDecorationCollection fontDecoration = textBox.TextDecorations;
+            if (textBox != null && fontDecoration != TextDecorations.Underline)
+            {
+                textBox.TextDecorations = TextDecorations.Underline;
+            }
+            else if (textBox != null && fontDecoration == TextDecorations.Underline)
+            {
+                textBox.TextDecorations = null;
+            }
+        }
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null && textBox.Foreground != Brushes.Black)
+            {
+                textBox.Foreground = Brushes.Black;
+            }
+        }
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null && textBox.Foreground != Brushes.Red)
+            {
+                textBox.Foreground = Brushes.Red;
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы(*.txt)|*.txt|Все файлы(*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы(*.txt)|*.txt|Все файлы(*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+            }
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Вам тут не справочная!", ":(", MessageBoxButton.OK, MessageBoxImage.Error);            
+        }
+    }
+}
---
+++ 02_ImageEditor/MainWindow.xaml.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _02_ImageEditor
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (inkCanvas != null)
+            {
+                inkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+            }
+        }
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            if (inkCanvas != null)
+            {
+                inkCanvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
+            }
+        }
+        private void RadioButton_Checked_6(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.EditingMode = InkCanvasEditingMode.Select;
+        }
+
+        private void RadioButton_Checked_2(object sender, RoutedEventArgs e)
+        {
+            if (inkCanvas != null)
+            {
+                inkCanvas.DefaultDrawingAttributes.Color = Colors.Black;
+            }
+        }
+
+        private void RadioButton_Checked_3(object sender, RoutedEventArgs e)
+        {
+            if (inkCanvas != null)
+            {
+                inkCanvas.DefaultDrawingAttributes.Color = Colors.Red;
+            }
+        }
+
+        private void RadioButton_Checked_4(object sender, RoutedEventArgs e)
+        {
+            if (inkCanvas != null)
+            {
+                inkCanvas.DefaultDrawingAttributes.Color = Colors.Green;
+            }
+        }
+
+        private void RadioButton_Checked_5(object sender, RoutedEventArgs e)
+        {
+            if (inkCanvas != null)
+            {
+                inkCanvas.DefaultDrawingAttributes.Color = Colors.Blue;
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int stylusWeigth = Convert.ToInt32(((sender as ComboBox).SelectedItem as TextBlock).Text);
+            if (inkCanvas != null)
+            {
+                inkCanvas.DefaultDrawingAttributes.Width = stylusWeigth;
+                inkCanvas.DefaultDrawingAttributes.Height = stylusWeigth;
+            }
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.Strokes.Clear();
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = new TextBox
+            {
+                Width = 100,
+                Height = 50,
+                BorderBrush = new SolidColorBrush(Colors.Transparent)
+            };
+            inkCanvas.Children.Add(textBox);
+            textBox.Focus();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Файлы изображений(*.jpg;*.jpeg,*.png,*.bmp)|*.jpg;*.jpeg,*.png,*.bmp|Все файлы(*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+
+            }
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Файлы изображений(*.bmp)|*.bmp|Все файлы(*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                int margin = (int)inkCanvas.Margin.Left;
+                int width = (int)inkCanvas.ActualWidth - margin;
+                int height = (int)inkCanvas.ActualHeight - margin;
+                RenderTargetBitmap bitmap = new RenderTargetBitmap(width, height, 96d, 96d, PixelFormats.Default);
+                bitmap.Render(inkCanvas);
+                string filename = saveFileDialog.FileName;
+                FileStream fileStream = new FileStream(filename, FileMode.Create);
+                using (fileStream)
+                {
+                    BmpBitmapEncoder encoder = new BmpBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create(bitmap));
+                    encoder.Save(fileStream);
+                }
+            }
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
