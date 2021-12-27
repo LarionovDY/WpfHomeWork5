2603c793b120a138f95e4ba07505e64767ec1131
Dmitriy Larionov <larionov.d.y@gmail.com>
2021-12-27 15:09:17 +0300

    доделал

--- 02_ImageEditor/MainWindow.xaml.cs
+++ 02_ImageEditor/MainWindow.xaml.cs
@@ -26,26 +26,26 @@ namespace _02_ImageEditor
         {
             InitializeComponent();
         }
-        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)      //выбор режима перо
         {
             if (inkCanvas != null)
             {
                 inkCanvas.EditingMode = InkCanvasEditingMode.Ink;
             }
         }
-        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)      //выбор режима ластик
         {
             if (inkCanvas != null)
             {
                 inkCanvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
             }
         }
-        private void RadioButton_Checked_6(object sender, RoutedEventArgs e)
+        private void RadioButton_Checked_6(object sender, RoutedEventArgs e)      //выбор режима dragndrop
         {
             inkCanvas.EditingMode = InkCanvasEditingMode.Select;
         }
 
-        private void RadioButton_Checked_2(object sender, RoutedEventArgs e)
+        private void RadioButton_Checked_2(object sender, RoutedEventArgs e)        //выбор черного пера
         {
             if (inkCanvas != null)
             {
@@ -53,7 +53,7 @@ namespace _02_ImageEditor
             }
         }
 
-        private void RadioButton_Checked_3(object sender, RoutedEventArgs e)
+        private void RadioButton_Checked_3(object sender, RoutedEventArgs e)        //выбор красного пера
         {
             if (inkCanvas != null)
             {
@@ -61,7 +61,7 @@ namespace _02_ImageEditor
             }
         }
 
-        private void RadioButton_Checked_4(object sender, RoutedEventArgs e)
+        private void RadioButton_Checked_4(object sender, RoutedEventArgs e)        //выбор зеленого пера
         {
             if (inkCanvas != null)
             {
@@ -69,7 +69,7 @@ namespace _02_ImageEditor
             }
         }
 
-        private void RadioButton_Checked_5(object sender, RoutedEventArgs e)
+        private void RadioButton_Checked_5(object sender, RoutedEventArgs e)        //выбор синего пера
         {
             if (inkCanvas != null)
             {
@@ -77,7 +77,7 @@ namespace _02_ImageEditor
             }
         }
 
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)      //выбор толщины пера
         {
             int stylusWeigth = Convert.ToInt32(((sender as ComboBox).SelectedItem as TextBlock).Text);
             if (inkCanvas != null)
@@ -87,12 +87,12 @@ namespace _02_ImageEditor
             }
         }
 
-        private void Button1_Click(object sender, RoutedEventArgs e)
+        private void Button1_Click(object sender, RoutedEventArgs e)        //кнопка очистить экран
         {
             inkCanvas.Strokes.Clear();
         }
 
-        private void Button2_Click(object sender, RoutedEventArgs e)
+        private void Button2_Click(object sender, RoutedEventArgs e)        //кнопка добавить текст
         {
             TextBox textBox = new TextBox
             {
@@ -102,31 +102,31 @@ namespace _02_ImageEditor
             };
             inkCanvas.Children.Add(textBox);
             textBox.Focus();
-        }
+        }       
 
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Файлы изображений(*.jpg;*.jpeg,*.png,*.bmp)|*.jpg;*.jpeg,*.png,*.bmp|Все файлы(*.*)|*.*";
-            if (openFileDialog.ShowDialog() == true)
-            {
-
-            }
-        }
-
-        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)     //сохранение файла в bmp
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             saveFileDialog.Filter = "Файлы изображений(*.bmp)|*.bmp|Все файлы(*.*)|*.*";
             if (saveFileDialog.ShowDialog() == true)
             {
+                //определение размеров изображения
                 int margin = (int)inkCanvas.Margin.Left;
                 int width = (int)inkCanvas.ActualWidth - margin;
                 int height = (int)inkCanvas.ActualHeight - margin;
+                //пребразование в растровое изображение
                 RenderTargetBitmap bitmap = new RenderTargetBitmap(width, height, 96d, 96d, PixelFormats.Default);
-                bitmap.Render(inkCanvas);
+                Rect bounds = VisualTreeHelper.GetDescendantBounds(inkCanvas);   
+                DrawingVisual drawingVisual = new DrawingVisual();
+                using (DrawingContext drawingContext = drawingVisual.RenderOpen())      
+                {
+                    VisualBrush visualBrush = new VisualBrush(inkCanvas);
+                    drawingContext.DrawRectangle(visualBrush, null, new Rect(new Point(), bounds.Size));
+                }
+                bitmap.Render(drawingVisual);
+                //запись в файл
                 string filename = saveFileDialog.FileName;
-                FileStream fileStream = new FileStream(filename, FileMode.Create);
+                FileStream fileStream = new FileStream(filename, FileMode.Create);      
                 using (fileStream)
                 {
                     BmpBitmapEncoder encoder = new BmpBitmapEncoder();
@@ -136,9 +136,14 @@ namespace _02_ImageEditor
             }
         }
 
-        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)     //кнопка закрыть
         {
             Application.Current.Shutdown();
+        }
+
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)     //кнопка справка
+        {
+            MessageBox.Show("Вам тут не справочная!", ":(", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
